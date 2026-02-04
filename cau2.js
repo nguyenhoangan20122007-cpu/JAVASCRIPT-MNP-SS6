e1da3a7ce37f7bddf5a0ae728bcf85060e3fd4e1
nguyenhoangan20122007-cpu <nguyenhoangan20122007@gmail.com>
2026-02-04 13:45:32 +0700

    Update cau2.js

--- cau2.js
+++ cau2.js
@@ -2,10 +2,10 @@ let Moneynumber = [100, 200, 300, 400];
 let sum = 0;
 
 for (let i = 0; i < Moneynumber.length; i = i + 2) {
-    sum += Moneynumber[i];
-        console.log(sum);
+    sum += Moneynumber[i];    
 
 }
+console.log(sum);
 console.log(`Sử dụng vòng lặp for...of để duyệt qua mảng và in ra trực tiếp giá trị của từng phần tử.`);
 
 for (const i of Moneynumber) {
